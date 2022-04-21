5d4d8acf0a8d751f2c2c4603d417ec83743828b5
Filip Sekulic <filip.sekulic@consensys.net>
2022-04-21 13:28:46 +0200

    Refactored the code

--- ui/store/actions.js
+++ ui/store/actions.js
@@ -3712,12 +3712,11 @@ export function cancelQRHardwareSignRequest() {
   };
 }
 
-export function addCustomNetworks(customRpc) {
-  console.log(customRpc);
+export function addCustomNetwork(customRpc) {
   return async (dispatch) => {
     try {
       dispatch(setNewCustomNetworkAdded(customRpc))
-      await promisifiedBackground.addCustomNetworks(customRpc);
+      await promisifiedBackground.addCustomNetwork(customRpc);
     } catch (error) {
       log.error(error);
       dispatch(displayWarning('Had a problem changing networks!'));
@@ -3728,7 +3727,7 @@ export function addCustomNetworks(customRpc) {
 export function requestUserApproval(customRpc) {
   return async (dispatch) => {
     try {
-      await promisifiedBackground.requestUserApprovalA(customRpc);
+      await promisifiedBackground.requestUserApproval(customRpc);
     } catch (error) {
       log.error(error);
       dispatch(displayWarning('Had a problem changing networks!'));
